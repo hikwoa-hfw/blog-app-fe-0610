127eb34573d742704506a52eee044567e07c8c48
hikwoa-hfw <hibbanhfw@gmail.com>
2025-04-22 14:23:30 +0700

    feat: keeplogin

--- src/hooks/api/auth/useLogin.ts
+++ src/hooks/api/auth/useLogin.ts
@@ -1,20 +1,27 @@
 "use client";
 
 import { axiosInstance } from "@/lib/axios";
+import { useAuthStore } from "@/stores/auth";
 import { User } from "@/types/user";
 import { useMutation } from "@tanstack/react-query";
 import { AxiosError } from "axios";
-import { toast } from "sonner"
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
 
 const useLogin = () => {
+  const { onAuthSuccess } = useAuthStore();
+  const router = useRouter()
+
   return useMutation({
     mutationFn: async (payload: Pick<User, "email" | "password">) => {
       const { data } = await axiosInstance.post("/auth/login", payload);
 
       return data;
     },
-    onSuccess: () => {
+    onSuccess: (data) => {
       toast.success("Login Success");
+      onAuthSuccess({ user: data.user , accessToken: data.accessToken });
+      router.push("/")
     },
     onError: (error: AxiosError<any>) => {
       toast.error(error.response?.data.message);
