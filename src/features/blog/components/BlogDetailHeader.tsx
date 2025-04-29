deae3a649ce53d780820768e825ed2a9aabdeaa4
hikwoa-hfw <hibbanhfw@gmail.com>
2025-04-29 15:25:37 +0700

    fix

--- src/features/blog/components/BlogDetailHeader.tsx
+++ src/features/blog/components/BlogDetailHeader.tsx
@@ -1,50 +1,45 @@
 import { Badge } from "@/components/ui/badge";
+import ModalDeleteBlog from "@/features/blog/components/ModalDeleteBlog";
 import useDeleteBlog from "@/hooks/api/blogs/useDeleteBlog";
 import { Blog } from "@/types/blog";
 import { format } from "date-fns";
 import { useSession } from "next-auth/react";
 import Image from "next/image";
 import { FC } from "react";
-import ModalDeleteBlog from "./ModalDeleteBlog";
 
 interface BlogDetailHeaderProps {
   blog: Blog;
 }
-
 const BlogDetailHeader: FC<BlogDetailHeaderProps> = ({ blog }) => {
-  const { mutateAsync: deleteBlog, isPending } = useDeleteBlog();
   const session = useSession();
 
-  const handleDelete = async () => {
+  const { mutateAsync: deleteBlog, isPending } = useDeleteBlog();
+  const handleDeleteBlog = async () => {
     await deleteBlog(blog.id);
   };
   return (
-    <section className="mt-10 space-y-2">
+    <section className="mt-20 space-y-4">
       <Badge
         variant="outline"
-        className="rounded-sm bg-green-100 text-gray-600 capitalize"
+        className="rounded-sm bg-green-100 text-green-600 capitalize"
       >
         {blog.category}
       </Badge>
-
       <h1 className="text-3xl font-bold">{blog.title}</h1>
-
       <div className="flex items-center justify-between">
         <p>
-          {format(new Date(blog.createdAt), "dd MM yyyy")} -{" "}
-          <span className="capitalize">{blog.user?.name}</span>
+          {format(new Date(blog.createdAt), "dd MM yyyy")}- {blog.user?.name}
         </p>
-        {Number(session.data?.user.id) === blog.userId && (
-          <ModalDeleteBlog isPending={isPending} onClick={handleDelete} />
+        {Number(session.data?.user?.id) === blog.userId && (
+          <ModalDeleteBlog isPending={isPending} onClick={handleDeleteBlog} />
         )}
       </div>
-
-      <div className="relative h-[300px]">
+      <div className="relative h-[240px] md:h-[360px]">
         <Image
           src={blog.thumbnail}
           alt="thumbnail"
-          fill
           className="rounded-sm object-cover"
+          fill
         />
       </div>
     </section>
