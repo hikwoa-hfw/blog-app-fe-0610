126f34dc0f4f7e721272b08e09978ee853dcac14
hikwoa-hfw <hibbanhfw@gmail.com>
2025-04-29 11:53:58 +0700

    xx

--- src/features/register/schemas.ts
+++ src/features/register/schemas.ts
@@ -5,5 +5,5 @@ YupPassword(Yup)
 export const RegisterSchema = Yup.object().shape({
     name: Yup.string().required("Name is required"),
     email: Yup.string().email("Invalid Email").required("Email is required"),
-    password: Yup.string().required("Password is required").minWords(6)
+    password: Yup.string().required("Password is required").min(6)
 })
